2e087d0a160ec7968b2c5c305ef7894a268fee7c
UdhayKrishn <krishnauday320@gmail.com>
2026-03-03 10:06:46 +0530

    feat(main): import global stylesheet in application entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from '@shopify/polaris';
 import '@shopify/polaris/build/esm/styles.css';
 import enTranslations from '@shopify/polaris/locales/en.json';
+import './index.css';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
