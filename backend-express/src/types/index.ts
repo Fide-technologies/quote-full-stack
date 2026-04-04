16ada08512845829b6a7d48af4611ed8bc6cfe28
uday <krishnauday320@gmail.com>
2026-04-05 04:46:32 +0530

    chore: formatting and project-wide cleanups in index.ts

--- backend-express/src/types/index.ts
+++ backend-express/src/types/index.ts
@@ -2,7 +2,20 @@ export { TYPES } from "./types";
 export type { IMerchant, MerchantDocument } from "./merchant.types";
 export type { IPlan, PlanDocument, IPlanFeatures } from "./plan.types";
 export type { IQuote, IQuoteItem, QuoteDocument } from "./quote.types";
-export type { GetSettingsResponse, GraphQLResponse, MetafieldDefinitionCreateResponse, MetafieldsSetResponse, ShopIdResponse } from "./settings.types";
-export type { DraftOrderLineItem, DraftOrderCustomer, DraftOrderShippingAddress, DraftOrderInput, DraftOrderCreateResponse, ShopResponse } from "./shopify.types";
+export type {
+    GetSettingsResponse,
+    GraphQLResponse,
+    MetafieldDefinitionCreateResponse,
+    MetafieldsSetResponse,
+    ShopIdResponse,
+} from "./settings.types";
+export type {
+    DraftOrderLineItem,
+    DraftOrderCustomer,
+    DraftOrderShippingAddress,
+    DraftOrderInput,
+    DraftOrderCreateResponse,
+    ShopResponse,
+} from "./shopify.types";
 export type { IForm, IFormStep, IFormField, FormDocument } from "./form.types";
 export { FormFieldType } from "./form.types";
