423f34a7216000e70e11b4b858cbfd914cb5fac6
uday <krishnauday320@gmail.com>
2026-04-05 04:46:31 +0530

    chore: formatting and project-wide cleanups in inversify.config.ts

--- backend-express/src/inversify.config.ts
+++ backend-express/src/inversify.config.ts
@@ -1,38 +1,49 @@
+import { PlanRepository } from "@/repositorys/plans";
 import { Container } from "inversify";
-import { TYPES } from "./types/types";
+import { AuthController } from "./controllers/auth.controller";
+import { DashboardController } from "./controllers/dashboard.controller";
+import { DraftOrderController } from "./controllers/draft-order.controller";
+import { FormController } from "./controllers/form.controller";
+import { MerchantController } from "./controllers/merchant.controller";
+import { PlanController } from "./controllers/plan.controller";
+import { QuoteController } from "./controllers/quote.controller";
+import { SettingsController } from "./controllers/settings.controller";
+import { UploadController } from "./controllers/upload.controller";
+import { WebhooksController } from "./controllers/webhoook.controller";
+import type { IFormRepository, IMerchantRepository, IPlanRepository, IQuoteRepository } from "./interfaces";
+import { DraftOrderMapper } from "./mappers/draft-order.mapper";
+import { FormRepository } from "./repositorys/forms";
 import { MerchantRepository } from "./repositorys/merchants";
 import { QuoteRepository } from "./repositorys/quotes";
-import { PlanRepository } from "@/repositorys/plans";
-import { MerchantService } from "./services/merchant.service";
-import { QuoteService } from "./services/quote.service";
-import { PlanService } from "./services/plan.service";
-import { EmailService } from "./services/email.service";
-import { SettingsService } from "./services/settings.service";
-import { DraftOrderService } from "./services/draft-order.service";
-import { UsageService } from "./services/usage.service";
-import { DraftOrderMapper } from "./mappers/draft-order.mapper";
-import { WebhookRegistry } from "./services/webhook-registry.service";
-import { AuthController } from "./controllers/auth.controller";
-import { QuoteController } from "./controllers/quote.controller";
-import { WebhooksController } from "./controllers/webhoook.controller";
-import { MerchantController } from "./controllers/merchant.controller";
-import { SettingsController } from "./controllers/settings.controller";
-import { DraftOrderController } from "./controllers/draft-order.controller";
-import { PlanController } from "./controllers/plan.controller";
-import { FormRepository } from "./repositorys/forms";
-import { FormService } from "./services/form.service";
-import { FormController } from "./controllers/form.controller";
-import { DashboardService } from "./services/dashboard.service";
-import { DashboardController } from "./controllers/dashboard.controller";
-import { UploadController } from "./controllers/upload.controller";
 import { CloudinaryUploadService } from "./services/cloudinary-upload.service";
+import { DashboardService } from "./services/dashboard.service";
+import { DraftOrderService } from "./services/draft-order.service";
+import { EmailService } from "./services/email.service";
+import { FormService } from "./services/form.service";
+import { MerchantService } from "./services/merchant.service";
+import { PlanService } from "./services/plan.service";
+import { QuoteService } from "./services/quote.service";
 import { S3UploadService } from "./services/s3-upload.service";
-import type { IMerchantRepository, IQuoteRepository, IPlanRepository, IFormRepository } from "./interfaces";
+import { SettingsService } from "./services/settings.service";
+import { UsageService } from "./services/usage.service";
+import { WebhookRegistry } from "./services/webhook-registry.service";
+import { TYPES } from "./types/types";
 
-import type { IMerchantService, IQuoteService, IPlanService, IEmailService, ISettingsService, IDraftOrderService, IWebhookRegistry, IUsageService, IFormService, IDashboardService, IUploadService } from "./interfaces";
+import type {
+    IDashboardService,
+    IDraftOrderService,
+    IEmailService,
+    IFormService,
+    IMerchantService,
+    IPlanService,
+    IQuoteService,
+    ISettingsService,
+    IUploadService,
+    IUsageService,
+    IWebhookRegistry,
+} from "./interfaces";
 
 const container = new Container();
-
 
 container.bind<IMerchantRepository>(TYPES.IMerchantRepository).to(MerchantRepository).inSingletonScope();
 container.bind<IQuoteRepository>(TYPES.IQuoteRepository).to(QuoteRepository).inSingletonScope();
@@ -67,6 +78,5 @@ container.bind<DashboardController>(TYPES.DashboardController).to(DashboardContr
 container.bind<IUploadService>(TYPES.IUploadService).to(CloudinaryUploadService).inSingletonScope();
 
 container.bind<UploadController>(TYPES.UploadController).to(UploadController).inSingletonScope();
-
 
 export { container };
